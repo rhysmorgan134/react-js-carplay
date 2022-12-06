0b4c9952e07c0cefc725bd84156f24fafc719da6
Rhys_M <rhys1802@hotmail.co.uk>
2022-12-06 20:25:10 +0000

    react 18 and webpack fixes

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,22 @@
+const webpack = require("webpack")
+
 module.exports = function override(config, env) {
-  console.log("React app rewired works!")
-  config.resolve.fallback = {
-    stream: false
-  };
-  return config;
-};
+    //do stuff with the webpack config...
+    config.resolve.fallback = {
+        ...config.resolve.fallback,
+        stream: require.resolve("stream-browserify"),
+        buffer: require.resolve("buffer"),
+    }
+    config.resolve.extensions = [...config.resolve.extensions, ".ts", ".js"]
+    config.plugins = [
+        ...config.plugins,
+        new webpack.ProvidePlugin({
+            process: "process/browser",
+            Buffer: ["buffer", "Buffer"],
+        }),
+    ]
+    // console.log(config.resolve)
+    // console.log(config.plugins)
+
+    return config
+}
